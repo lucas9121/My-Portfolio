5bbc352065937291035fcfa16e419a6dc7d11983
Lucas <lucas2carlos@gmail.com>
2022-06-13 20:48:33 -0400

    updating event listener

--- js/script.js
+++ js/script.js
@@ -10,29 +10,23 @@ const icons = document.querySelectorAll('.icon')
 
 input.addEventListener('click', () => {
     if(input.checked){
-        setTimeout(() => {
-            body.classList.remove('night-animation')
-            body.style.background = 'linear-gradient(60deg, rgb(0 0 0 / 60%), rgb(0 0 0 / 80%), rgb(1 18 42 / 70%), rgb(1 18 42 / 60%), rgb(1 18 42 / 60%), rgb(1 18 42 / 60%), rgb(1 18 42 / 60%), rgb(1 18 42 / 70%), rgb(0 62 129))'
-            html.style.background = '#080808'
-        }, 1000)
-        body.classList.add('night-animation')
-        body.classList.add('transition-effect')
+        body.style.background = 'linear-gradient(60deg, rgb(0 0 0 / 60%), rgb(0 0 0 / 80%), rgb(1 18 42 / 70%), rgb(1 18 42 / 60%), rgb(1 18 42 / 60%), rgb(1 18 42 / 60%), rgb(1 18 42 / 60%), rgb(1 18 42 / 70%), rgb(0 62 129))'
+        html.style.background = '#080808'
         body.style.color = 'rgb(255 255 255 / 78%)'
         header.style.background = 'rgb(0 62 129 / 70%)'
         footer.style.background = 'rgb(87 96 109 / 30%)'
+        console.log(icons)
         for(let i = 0; i < icons.length; i++){
-            icons[i].classList.add('darkmode-icons')
+            if(i > 5){
+                icons[i].classList.add('darkmode-icons')
+
+            }
             if(i <= links.length - 1) links[i].classList.add('darkmode-links')
-            // if(i <= projects.length - 1) projects[i].style.background = 'rgb(0, 0, 0, 0.7)'
             footer.style.background = 'rgb(87 96 109 / 30%)'
         }
     } else {
-        setTimeout(() => {
-            body.classList.remove('day-animation')
-            body.style.background = ''
-            html.style.background = ''
-        }, 1000)
-        body.classList.add('day-animation')
+        body.style.background = ''
+        html.style.background = ''
         body.style.color = ''
         header.style.background = ''
         footer.style.background = ''
